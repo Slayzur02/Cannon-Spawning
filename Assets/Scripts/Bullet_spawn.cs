78ca4330ddb6411f78dddec3a144d4687df52131
Slayzur <ducanh.tranpro@gmail.com>
2019-09-08 17:44:02 +0700

    something something

--- Assets/Scripts/Bullet_spawn.cs
+++ Assets/Scripts/Bullet_spawn.cs
@@ -2,22 +2,24 @@
 using System.Collections;
 
 public class Bullet_spawn : MonoBehaviour
-{ 
+{   public float bulletWaitTime;
     public GameObject bullet;
     public Transform spawnpoint;
     public float bulletSpeed;
     public Vector3 direction;
 
     void Start() {
-    direction = new Vector3(0.0f, 0.0f, Random.Range(160.0f, 200f));
-    bulletSpeed = Random.Range(4.0f, 8.0f);
-    StartCoroutine(Countdown1());
+        bulletWaitTime = Random.Range(0.8f, 1.5f);
+        direction = new Vector3(0.0f, 0.0f, Random.Range(160.0f, 200f));
+        bulletSpeed = Random.Range(5.0f, 15.0f);
+        StartCoroutine(Countdown1());
     }
 
     private IEnumerator Countdown1() {
     while(true) {
-        yield return new WaitForSeconds(0.7f); 
-        Instantiate(bullet, spawnpoint.position, spawnpoint.rotation);
+        yield return new WaitForSeconds(bulletWaitTime); 
+        var callingBullet = Instantiate(bullet, spawnpoint.position, spawnpoint.rotation);
+        callingBullet.GetComponent<Rigidbody2D>().velocity  = callingBullet.transform.right * bulletSpeed;
     }
     }
 
